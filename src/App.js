a35e70f46d97a57859ac4d0c4f604e158fa7595d
Glen Alloy <glenalloy8@gmail.com>
2024-07-10 11:43:10 +0530

    created Navigation.js file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends Component{
+  render() {
+    return (
+      <div className="App">
+        <Navigation />
+        {/*<Logo />
+        <ImageLinkForm />
+    <FaceRecognition />*/}
+      </div>
+    );
+  }
+  
 }
 
 export default App;
