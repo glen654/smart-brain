3a8d9f3312e5e78f78b9881955d87679fa74259a
Glen Alloy <glenalloy8@gmail.com>
2024-07-11 11:04:22 +0530

    making the cursor pointer

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,5 +1,5 @@
 import React from "react";
-import './ImageLinkForm.css'
+import './ImageLinkForm.css';
 
 const ImageLinkForm = () => {
     return (
@@ -8,7 +8,7 @@ const ImageLinkForm = () => {
                 {'This Magic will detect faces in your pictures. Give it a try'}
             </p>
             <div className="center">
-                <div className=" form pa4 br3 shadow-5">
+                <div className=" form center pa4 br3 shadow-5">
                   <input className="fa4 pa2 w-70 center" type='tex'/>
                   <button className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple">Detect</button>
                 </div>
