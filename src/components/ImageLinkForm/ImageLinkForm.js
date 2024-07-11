f5843998c807f7cd0ccdb61ec8a2b94ea1a2d717
Glen Alloy <glenalloy8@gmail.com>
2024-07-11 11:33:50 +0530

    added background to the app

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -4,7 +4,7 @@ import './ImageLinkForm.css';
 const ImageLinkForm = () => {
     return (
         <div>
-            <p className="f3">
+            <p className="white f3">
                 {'This Magic will detect faces in your pictures. Give it a try'}
             </p>
             <div className="center">
