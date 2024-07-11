bfcf06963bddedb3e1d02a0af8c20987444824d9
Glen Alloy <glenalloy8@gmail.com>
2024-07-11 10:27:58 +0530

    created Logo.css file

--- src/components/logo/Logo.js
+++ src/components/logo/Logo.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Tilt from 'react-parallax-tilt';
+import './Logo.css';
 
 const Logo = () => {
     return (
         <div className="ma4 mt0" style={{width: '150px', height: '150px'}}>
             <Tilt>
-             <div className="shadow-1" style={{ width: '150px', height: '150px', backgroundColor: 'transparent' }}>
+             <div className="shadow-1" options={{max: 55}} style={{ width: '150px', height: '150px'}}>
                 <h1>React Parallax Tilt 👀</h1>
             </div>
             </Tilt>
