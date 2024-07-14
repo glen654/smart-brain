742f61ab04f3d55e60b43e05c9e96499850f1cbc
Glen Alloy <glenalloy8@gmail.com>
2024-07-14 12:05:56 +0530

    added a sample image to the face recognition box

---
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const FaceRecognition = () => {
+    return (
+        <div className="center">
+            <img alt='image' src={'https://upload.wikimedia.org/wikipedia/commons/6/68/The_Smiths_%281984_Sire_publicity_photo%29_002.jpg'} />
+        </div>
+    );
+}
+
+export default FaceRecognition;
