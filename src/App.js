1dede6ad96118b92d8e6059602714efcff702378
Glen Alloy <glenalloy8@gmail.com>
2024-07-15 12:01:50 +0530

    created setUpClarifai function

--- src/App.js
+++ src/App.js
@@ -7,15 +7,17 @@ import Rank from './components/Rank/Rank';
 import ParticlesBg from 'particles-bg';
 import './App.css';
 
-const PAT = 'YOUR_PAT_HERE';
+const setUpClarifai =(imageUrl) =>{
+    const PAT = 'a1f9cfd6934d4a279577e28985f79d77';
     // Specify the correct user_id/app_id pairings
     // Since you're making inferences outside your app's scope
-    const USER_ID = 'clarifai';       
-    const APP_ID = 'main';
+    const USER_ID = '82nxn1prhu8q';       
+    const APP_ID = 'my-first-application-e38qz';
     // Change these to whatever model and image URL you want to use
-    const MODEL_ID = 'general-image-recognition';
-    const MODEL_VERSION_ID = 'aa7f35c01e0642fda5cf400f543e7c40';    
+    const MODEL_ID = 'face-detection';   
     const IMAGE_URL = 'https://samples.clarifai.com/metro-north.jpg';
+}
+
 
     ///////////////////////////////////////////////////////////////////////////////////
     // YOU DO NOT NEED TO CHANGE ANYTHING BELOW THIS LINE TO RUN THIS EXAMPLE
@@ -54,7 +56,7 @@ const PAT = 'YOUR_PAT_HERE';
         .then(response => response.text())
         .then(result => console.log(result))
         .catch(error => console.log('error', error));
-        
+
 class App extends Component{
   constructor(){
     super();
