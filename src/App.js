dbd5b6505ffe1ffed3ac94953ac0be8e31bae677
Glen Alloy <glenalloy8@gmail.com>
2024-07-16 19:58:11 +0530

    calculating the face detection box

--- src/App.js
+++ src/App.js
@@ -59,7 +59,14 @@ class App extends Component{
 
   calculateFaceLocation = (data) => {
     const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
-    const image = document.getElementById('inputImage')
+    const image = document.getElementById('inputImage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return{
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width)
+    }
   }
 
   onInputChange = (event) => {
