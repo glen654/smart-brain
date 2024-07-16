e6f24879fe9c8a5158cd9a25b846b40c33b98eb9
Glen Alloy <glenalloy8@gmail.com>
2024-07-16 21:35:16 +0530

    signin css created

---
+++ src/components/SignIn/SignIn.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './SignIn.css';
+
+const SignIn = ({imageUrl, box}) => {
+    return (
+        <div>
+            
+            
+        </div>
+    );
+}
+
+export default SignIn;
