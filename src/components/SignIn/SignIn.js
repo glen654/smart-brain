a14bf83130c75c4e38362f34c0eb5eb2fe1f53f2
Glen Alloy <glenalloy8@gmail.com>
2024-07-17 11:02:58 +0530

    changed the font size of the sign in

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -7,7 +7,7 @@ const SignIn = () => {
         <main className="pa4 white-80">
             <form className="measure">
                 <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-                <legend className="f4 fw6 ph0 mh0">Sign In</legend>
+                <legend className="f1 fw6 ph0 mh0">Sign In</legend>
                 <div className="mt3">
                     <label className="db fw6 lh-copy f6" for="email-address">Email</label>
                     <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address" />
@@ -21,7 +21,7 @@ const SignIn = () => {
                     <input className="b ph3 pv2 input-reset ba b--white bg-transparent grow pointer f6 dib white" type="submit" value="Sign in" />
                 </div>
                 <div className="lh-copy mt3">
-                    <a href="#0" class="f6 link dim white db">Sign up</a>
+                    <a href="#0" class="f6 link dim white db">Register</a>
                 </div>
             </form>
         </main>
