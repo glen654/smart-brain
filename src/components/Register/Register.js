b7e0247cb0c99dff54592a88d093204d5d770354
Glen Alloy <glenalloy8@gmail.com>
2024-09-25 12:45:08 +0530

    changed some styles in register js

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -51,15 +51,15 @@ class Register extends React.Component {
                         <legend className="f1 fw6 ph0 mh0">Register</legend>
                         <div className="mt3">
                             <label className="db fw6 lh-copy f6" htmlFor="name">Name</label>
-                            <input className="pa2 input-reset ba bg-transparent hover-bg-transparent hover-white w-100" type="text" name="name"  id="name" onChange={this.onNameChange} style={{textTransform:'lowercase'}}/>
+                            <input className="pa2 input-reset ba bg-transparent hover-bg-transparent hover-white w-100" type="text" name="name"  id="name" onChange={this.onNameChange} />
                         </div>
                         <div className="mt3">
                             <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
-                            <input className="pa2 input-reset ba bg-transparent hover-bg-transparent hover-white w-100" type="email" name="email-address"  id="email-address" onChange={this.onEmailChange} style={{textTransform:'lowercase'}}/>
+                            <input className="pa2 input-reset ba bg-transparent hover-bg-transparent hover-white w-100" type="email" name="email-address"  id="email-address" onChange={this.onEmailChange} />
                         </div>
                         <div className="mv3">
                             <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
-                            <input className="b pa2 input-reset ba bg-transparent hover-bg-transparent hover-white w-100" type="password" name="password"  id="password" onChange={this.onPasswordChange} style={{textTransform:'lowercase'}}/>
+                            <input className="b pa2 input-reset ba bg-transparent hover-bg-transparent hover-white w-100" type="password" name="password"  id="password" onChange={this.onPasswordChange} />
                         </div>
                         </fieldset>
                         <div className="">
